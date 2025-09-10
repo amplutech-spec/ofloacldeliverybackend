ab71a6b90d953bd0c7bb70bd3686bb3111f24245
amplutech-spec <amplutech@gmail.com>
2025-09-10 19:05:54 +0530

    Message1

--- models/User.js
+++ models/User.js
@@ -3,12 +3,12 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  number: { type: String, required: true },
-  password: { type: String, required: true },
+  number: { type: String, required: function () { return this.loginType !== 'Google'; } }, // only required if not Google login
+  password: { type: String, required: function () { return this.loginType !== 'Google'; } }, // skip for Google
   token: { type: String },
   photoUrl: { type: String },
-  available: { type: String },
-  loginType: { type: String }
-}, { timestamps: true });
+  available: { type: String, default: "No" },
+  loginType: { type: String, enum: ["Normal", "Google"], default: "Normal" }
+});
 
 module.exports = mongoose.model("User", userSchema);
